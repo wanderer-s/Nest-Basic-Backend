e9c5e9a8ed63d3d451fb7f00b62f91b47723f420
Whatseop <ows@lugstay.com>
2022-01-05 23:27:00 +0900

    Nest basic set

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.use(helmet());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true
+    })
+  );
+
+  const swaggerConfig = new DocumentBuilder().setTitle('Api 명세').setVersion('0.0.0').build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('doc', app, swaggerDocument, {
+    swaggerOptions: { defaultModelsExpandDepth: -1 }
+  });
+  await app.listen(3000);
+}
+bootstrap();
