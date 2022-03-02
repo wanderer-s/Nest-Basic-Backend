77a591e258a9ad0f58fd3b2c3d8768ba4e6f2037
wanderer-s <ows0871@gmail.com>
2022-03-03 01:06:25 +0900

    App controllers, services 제거

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,10 @@
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService]
+  imports: [UsersModule, AuthModule]
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
