3f0052cc9c4df2f35ce30afd5bacdd020aba6801
wanderer-s <ows0871@gmail.com>
2022-03-07 22:23:23 +0900

    password update api

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { ApiBadRequestResponse, ApiInternalServerErrorResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Param, Patch, Post, Req, UseGuards } from '@nestjs/common';
+import { ApiBadRequestResponse, ApiBearerAuth, ApiForbiddenResponse, ApiInternalServerErrorResponse, ApiOperation, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/jwt/jwt.guard';
+import { PasswordUpdateDto } from './dto/password.update.dto';
 import { UserCreateDto } from './dto/user.create.dto';
 import { UsersService } from './users.service';
 
@@ -23,5 +25,28 @@ export class UsersController {
   @Post('')
   async signUp(@Body() dto: UserCreateDto) {
     return await this.usersService.signUp(dto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  //
+  @ApiOperation({
+    summary: '비밀번호 변경',
+    description: '## 사용자 비밀번호 변경'
+  })
+  @ApiUnauthorizedResponse({
+    description: '- `Unauthorized`'
+  })
+  @ApiBadRequestResponse({
+    description:
+      '- `Password must follow the rule` 비밀번호는 8~20자리이며 숫자와 문자 최소 1개 포함\n- `newPassword and newPasswordCheck must be same` 비밀번호와 비밀번호확인 불일치'
+  })
+  @ApiForbiddenResponse({
+    description: '- `Access is denied` 잘못된 접근'
+  })
+  //
+  @Patch('password')
+  async passwordUpdate(@Body() dto: PasswordUpdateDto, @Req() req): Promise<void> {
+    await this.usersService.passwordUpdate(req.user.id, dto);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,8 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, ForbiddenException, Injectable } from '@nestjs/common';
 import { UserCreateDto } from './dto/user.create.dto';
 import { PrismaService } from 'prisma/prisma.service';
 import bcrypt from 'bcrypt';
+import { PasswordUpdateDto } from './dto/password.update.dto';
 
 @Injectable()
 export class UsersService {
@@ -37,5 +38,19 @@ export class UsersService {
     const hashedPassword = await bcrypt.hash(password, 10);
 
     await this.prisma.users.create({ data: { email, nickname, password: hashedPassword } });
+  }
+
+  async passwordUpdate(userId, dto: PasswordUpdateDto) {
+    const foundUser = await this.prisma.users.findUnique({ where: { id: userId } });
+
+    if (!foundUser) throw new ForbiddenException('Access is denied');
+
+    this.validatePassword(dto.newPassword, dto.newPasswordCheck);
+
+    if (!(await bcrypt.compare(dto.password, foundUser.password))) throw new BadRequestException('Wrong password');
+
+    const hashedPassword = await bcrypt.hash(dto.newPassword, 10);
+
+    await this.prisma.users.update({ where: { id: userId }, data: { password: hashedPassword } });
   }
 }
