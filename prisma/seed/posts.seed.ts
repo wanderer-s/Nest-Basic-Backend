3cb21dd037a895d641f7c047d1de09317d85d525
wanderer-s <ows0871@gmail.com>
2022-03-13 00:40:07 +0900

    posts seed file

---
+++ prisma/seed/posts.seed.ts
@@ -0,0 +1,14 @@
+export const posts = [
+  {
+    id: 1,
+    title: 'First Post',
+    content: 'This is dummy data.',
+    userId: 1
+  },
+  {
+    id: 2,
+    title: 'Second Post',
+    content: 'This is dummy data.',
+    userId: 1
+  }
+];
