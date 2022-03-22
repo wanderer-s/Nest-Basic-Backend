3107cb6ecb7f8214536be5c7300e373607ba9257
Whatseop <ows0871@gmail.com>
2022-03-23 01:32:01 +0900

    Add post comment api

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,9 +1,39 @@
-import { Controller, Get, Param } from '@nestjs/common';
-import { ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, Param, ParseIntPipe, Post, Query, Req, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiNotFoundResponse, ApiOkResponse, ApiOperation, ApiParam, ApiQuery, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/jwt/jwt.guard';
 import { CommentsService } from './comments.service';
+import { CommentCreateDto } from './dto/comment.create.dto';
+import { CommentsResponseDto } from './dto/comments.response.dto';
 
 @Controller('posts/:id/comments')
 @ApiTags('Posts')
 export class CommentsController {
   constructor(private readonly commentsService: CommentsService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  //
+  @ApiOperation({
+    summary: '댓글 남기기',
+    description: '## 게시글에 댓글 남기기'
+  })
+  @ApiParam({
+    name: 'id',
+    description: '게시글 id',
+    schema: {
+      type: 'integer',
+      example: 1
+    }
+  })
+  @ApiUnauthorizedResponse({
+    description: '- `Unauthorized`'
+  })
+  @ApiNotFoundResponse({
+    description: "- `Couldn't find post` 주어진 id로 게시글을 찾을 수 없음"
+  })
+  //
+  @Post()
+  async createComment(@Param('id') postId: number, @Body() dto: CommentCreateDto, @Req() req) {
+    await this.commentsService.createComment(postId, dto, req.user.id)
+  }
 }
--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,7 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'prisma/prisma.service';
+import { CommentCreateDto } from './dto/comment.create.dto';
 
 @Injectable()
 export class CommentsService {
   constructor(private readonly prisma: PrismaService) {}
+
+  async getPostById(postId: number) {
+    const foundPost = await this.prisma.posts.findUnique({where: {id: postId}})
+    if(!foundPost) throw new NotFoundException("Couldn't find post")
+    
+    return foundPost
+  }
+
+  async createComment(postId: number, dto: CommentCreateDto, userId: number) {
+    await this.getPostById(postId)
+
+    const data = {postId, ...dto, userId}
+    await this.prisma.comments.create({data})
+  }
 }
