5621083e0b7ed50d6b515906d8f0f7f52209b489
wanderer-s <ows0871@gmail.com>
2022-04-19 22:34:44 +0900

    Add comments repository

---
+++ src/comments/repository/abstract.comments.repository.ts
@@ -0,0 +1,9 @@
+import { Comments, Prisma } from '@prisma/client';
+
+export abstract class AbstractCommentsRepository {
+  abstract createComment(data: Prisma.CommentsUncheckedCreateInput): Promise<Comments>
+
+  abstract getComments(postId: number, take: number, lastCommentId?: number): Promise<Comments[]>
+
+  abstract updateComment(commentId: number, data: Prisma.CommentsUpdateInput): Promise<Comments>
+}
