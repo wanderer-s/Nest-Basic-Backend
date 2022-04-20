0ed9c6e06162401c1d408bc69691b460f7d622ff
wanderer-s <ows0871@gmail.com>
2022-04-20 13:36:36 +0900

    Refectoring posts, comments

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { CommentsController } from './comments.controller';
 import { PrismaService } from '../common/prisma.service';
+import { PostsModule } from '../posts/posts.module';
+import { CommentsRepository } from './repository/comments.repository';
 
 @Module({
-  providers: [CommentsService, PrismaService],
+  imports: [PostsModule],
+  providers: [CommentsService, CommentsRepository, PrismaService],
   controllers: [CommentsController]
 })
 export class CommentsModule {}
--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
 import { PrismaService } from '../common/prisma.service';
+import { PostsRepository } from './repository/posts.repository';
 
 @Module({
-  providers: [PostsService, PrismaService],
-  controllers: [PostsController]
+  providers: [PostsService, PostsRepository, PrismaService],
+  controllers: [PostsController],
+  exports: [PostsRepository]
 })
 export class PostsModule {}
